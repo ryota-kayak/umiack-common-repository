67c8d54a77e19ec53aa837ae5ee1766ffb533c53
ryota-kayak <sable-55skyway@icloud.com>
2026-04-16 18:37:57 +0900

    Cleanup: Remove redundant entry CSS file

--- src/wordpress/Kayak Tours/kayak-tours-loader.js
+++ src/wordpress/Kayak Tours/kayak-tours-loader.js
@@ -1,16 +1,27 @@
-// Kayak Tours loader
+// Umiack Kayak Tours Loader - Latest Version
 // Paste this into Cocoon's Custom JavaScript code field.
 (function () {
-  var base = '/common/umiack-site-assets/js/';
+  var baseJs = 'https://jp.umiack.com/common/umiack-site-assets/js/';
+  var baseCss = 'https://jp.umiack.com/common/umiack-site-assets/css/';
+  
+  // 1. Load Slider CSS (if not already present)
+  if (!document.getElementById('umiack-slider-css')) {
+    var l = document.createElement('link');
+    l.id = 'umiack-slider-css';
+    l.rel = 'stylesheet';
+    l.href = baseCss + 'kayak-tour.css';
+    document.head.appendChild(l);
+  }
+
+  // 2. Load Slider JS
   var files = [
     'umiack-slider.js'
   ];
 
   files.forEach(function (file) {
     var s = document.createElement('script');
-    s.src = base + file;
+    s.src = baseJs + file;
     s.defer = true;
     document.head.appendChild(s);
   });
 })();
-

97b9db3a3e7e4a8059fea00e64b6816a6a2331b6
ryota-kayak <sable-55skyway@icloud.com>
2026-04-26 14:18:36 +0900

    feat: include CSS loading with cache-busting in top page loaders

--- src/server/common/umiack-site-assets/js/top-loader-umiack.js
+++ src/server/common/umiack-site-assets/js/top-loader-umiack.js
@@ -1,16 +1,31 @@
 // umiack.com / jp.umiack.com top page loader
 (function () {
   var v = 'BUILD_VERSION';
-  var base = '/common/umiack-site-assets/js/';
-  var files = [
+  var jsBase = '/common/umiack-site-assets/js/';
+  var cssBase = '/common/umiack-site-assets/css/';
+
+  // 1. Load Styles
+  var styles = [
+    'top-base.css',
+    'top-umiack-logo.css',
+    'top-umiack-links.css'
+  ];
+  styles.forEach(function (file) {
+    var l = document.createElement('link');
+    l.rel = 'stylesheet';
+    l.href = cssBase + file + '?v=' + v;
+    document.head.appendChild(l);
+  });
+
+  // 2. Load Scripts
+  var scripts = [
     'top-shared.js',
     'top-backgrounds.js',
     'top-umiack-logo.js'
   ];
-
-  files.forEach(function (file) {
+  scripts.forEach(function (file) {
     var s = document.createElement('script');
-    s.src = base + file + '?v=' + v;
+    s.src = jsBase + file + '?v=' + v;
     s.defer = true;
     document.head.appendChild(s);
   });

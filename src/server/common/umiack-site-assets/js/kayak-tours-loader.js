ab0872382374d8ff5987f8a2fa2fcce6506c7178
ryota-kayak <sable-55skyway@icloud.com>
2026-04-26 14:58:39 +0900

    feat: move CSS loading to HTML in all Kayak Tours to prevent FOUC

--- src/server/common/umiack-site-assets/js/kayak-tours-loader.js
+++ src/server/common/umiack-site-assets/js/kayak-tours-loader.js
@@ -2,19 +2,9 @@
 // Centralized loader. Call this script from your WordPress page.
 (function () {
   var baseJs = '/common/umiack-site-assets/js/';
-  var baseCss = '/common/umiack-site-assets/css/';
   var version = 'BUILD_VERSION'; // Will be replaced during build
 
-  // 1. Load Styles (if not already present)
-  if (!document.getElementById('kayak-tour-css')) {
-    var l = document.createElement('link');
-    l.id = 'kayak-tour-css';
-    l.rel = 'stylesheet';
-    l.href = baseCss + 'kayak-tour.css?v=' + version;
-    document.head.appendChild(l);
-  }
-
-  // 2. Load Slider JS
+  // 1. Load Slider JS
   var files = [
     'umiack-slider.js'
   ];
